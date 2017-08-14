ba0f1fc86b6774b1b3d6b53f4d015af183b6ad80
Finn Terdal <fterdal@gmail.com>
2017-08-14 16:17:19 -0500

    AGGH nothing works

--- index.js
+++ index.js
@@ -23,25 +23,98 @@ for (let i=0; i<answersLength; i++) {
   let newNumber = Math.random() > 0.5 ? 2 : 1;
   answerArray.push( newNumber  );
 }
-console.log(answerArray);
+
+// function addAnswerToArray(arr) {
+//   arr.push(Math.random() > 0.5 ? 2 : 1)
+//   return arr;
+// }
+
+let userCorrect = true;
+
+answerArray = [1, 2, 2, 1, 1, 1, 2];
+
+function playSound (index, prevSound) {
+  let oneNum = answerArray[index];
+  if(prevSound){
+    prevSound.removeEventListener('ended');
+  }
+  if (oneNum) {
+    let sound;
+    if(oneNum === 1) sound = oneSound;
+    else sound = twoSound;
+    console.log(sound);
+    console.log(index);
+    // sound.removeEventListener();
+    sound.play();
+    sound.on('ended', function() {
+      playSound(index+1, sound);
+    })
+  }
+}
+playSound(0);
+
+// function playOne() {
+//   oneSound.play();
+//   oneSound.on('ended', function(seconds) {
+//     twoSound.play();
+//   });
+// }
+// function playOne() {
+//   oneSound.play();
+// }
+// function playTwo() {
+//   // twoSound.on('ended', function(seconds) {
+//   //   correctSound.play();
+//   // });
+//   twoSound.play();
+// }
+// playOne();
+// playTwo();
+
+// Step one: Read out the values to the user.
+// For Loop
+// for(var i = 0; i < answerArray.length; i+=2){
+//   let playNext =(answerArray[i+1] === 1) ? playOne : playTwo;
+//   if(answerArray[i] === 1){
+//     // console.log(nextSound);
+//     playOne(playNext);
+//   } else {
+//     // console.log(nextSound);
+//     playTwo(playNext);
+//   }
+// }
+
+// Step Two: User inputs and compare inputs to answers
+// For Loop
+
+// Step Three: If users screwed up, give them the buzzer. Game Over.
+// Otherwise, give them the correct sound. (Extra Credit: create another round
+// by appending new answers to the answers array)
+//
 
 
-oneSound.play();
-oneSound.on('ended', function(seconds) {
-  twoSound.play();
-});
-
-twoSound.play();
-twoSound.on('ended', function(seconds) {
-  correctSound.play();
-});
-
-correctSound.play();
-correctSound.on('ended', function(seconds) {
-  buzzerSound.play();
-});
-
-buzzerSound.play();
+console.log(answerArray)
+//
+// function playOne() {
+// oneSound.play();
+// oneSound.on('ended', function(seconds) {
+//   twoSound.play();
+// });
+// }
+//
+// function playTwo() {
+// twoSound.play();
+// twoSound.on('ended', function(seconds) {
+//   correctSound.play();
+// });
+// }
+//
+// correctSound.play();
+// correctSound.on('ended', function(seconds) {
+//   buzzerSound.play();
+// });
+//
+// buzzerSound.play();
 
 // Turn one of the LEDs on to start.
 // tessel.led[2].on();

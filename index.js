e70289c6bbd4b6c04e23e71bedd5b1262ce2b9a0
Finn Terdal <fterdal@gmail.com>
2017-08-14 14:49:54 -0500

    adds abilito to output all sounds

--- index.js
+++ index.js
@@ -2,14 +2,45 @@
 
 // Import the interface to Tessel hardware
 const tessel = require('tessel');
+const path = require('path');
+const av = require('tessel-av');
+
+// Import all the audio files
+const one = path.join(__dirname, 'one.mp3');
+const two = path.join(__dirname, 'two.mp3');
+const correct = path.join(__dirname, 'correct.mp3');
+const buzzer = path.join(__dirname, 'buzzer.mp3');
+
+// Convert them to a.Player sounds
+const oneSound = new av.Player(one);
+const twoSound = new av.Player(two);
+const correctSound = new av.Player(correct);
+const buzzerSound = new av.Player(buzzer);
+
+oneSound.play();
+oneSound.on('ended', function(seconds) {
+  twoSound.play();
+});
+
+twoSound.play();
+twoSound.on('ended', function(seconds) {
+  correctSound.play();
+});
+
+correctSound.play();
+correctSound.on('ended', function(seconds) {
+  buzzerSound.play();
+});
+
+buzzerSound.play();
 
 // Turn one of the LEDs on to start.
 // tessel.led[2].on();
 
 // Blink!
-setInterval(() => {
-  tessel.led[2].toggle();
-  tessel.led[3].toggle();
-}, 1000);
+// setInterval(() => {
+//   tessel.led[2].toggle();
+//   tessel.led[3].toggle();
+// }, 1000);
 
-console.log("I'm blinking! (Press CTRL + C to stop)");
+// console.log("I'm blinking! (Press CTRL + C to stop)");
